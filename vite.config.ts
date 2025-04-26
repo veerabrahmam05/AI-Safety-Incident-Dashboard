58d7ba0b628b91f6cc47de4f13f2ec1c94dc9dc0
veerabrahmam05 <veerabrahmamaluri@gmail.com>
2025-04-26 15:33:18 +0530

    after deploy

--- vite.config.ts
+++ vite.config.ts
@@ -3,8 +3,13 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/AI Safety Incident Dashboard/',
   plugins: [react()],
   optimizeDeps: {
     exclude: ['lucide-react'],
   },
+  build: {
+    outDir: 'dist',
+    sourcemap: true
+  }
 });
